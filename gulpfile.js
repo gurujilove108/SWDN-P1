4ed4b9c510e71c04e5513142408e81fc9f9e0f1d
Dylan Tucker Dannenhauer <dylan.dannenhauer@gmail.com>
2016-08-18 00:46:15 -0700

    live editing working

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
+var browserSync = require("browser-sync");
 
 gulp.task('default', function() {
-	console.log("default task running");
+	browserSync.init(null, {
+        proxy: "localhost:8080"
+    });
+
+    gulp.watch("static/js/*.js").on('change', browserSync.reload);
+    gulp.watch("static/css/*.css").on('change', browserSync.reload);
+    gulp.watch("templates/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('first_gulp_task_ever', function() {
-	console.log("First custom gulp task.. #Winning");
-});
+
