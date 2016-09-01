c7f8aa2e4f3c975e937c8e3fc5011ec76aeae391
Dylan Tucker Dannenhauer <dylan.dannenhauer@gmail.com>
2016-09-01 16:31:39 -0700

    added comments to event controller and reorganize how events are displayed

--- static/js/event_controller.js
+++ static/js/event_controller.js
@@ -14,6 +14,10 @@ controllers.controller('EventController', function ($scope, $location, oauth2Pro
 		return true;
 	}
 
+	/*
+	 * method to create the form rpc object to pass in to our create event api method
+	 * the keys in tje object must have the same name as the keys in the rpc object that is beint accepted by the api endpoint 
+    */
 	$scope.sendGapiCreateForm = function() {
 		var guestlist = document.getElementById("guestlist");
 		var guestMessage = document.getElementById("guest-message");
@@ -26,6 +30,7 @@ controllers.controller('EventController', function ($scope, $location, oauth2Pro
 			event_end:   jQuery("#event-datetime-end").val()
 		};
 
+		/* creating an array of guests to store in the Event database from the datalist in the create event form */
 		if (guestlist.childElementCount > 0) 
 			for (var i=0, guests=[], len=guestlist.children.length; i < len; i++) {
 				var current_guest = guestlist.children[i].value;
@@ -43,6 +48,7 @@ controllers.controller('EventController', function ($scope, $location, oauth2Pro
 
 	}
 
+	/* This function allows a user to add a guest to the data list */
 	$scope.addGuest = function() {
 		if (defined($scope.current_guest)) {
 			var guestlist = jQuery("#guestlist");
@@ -53,47 +59,65 @@ controllers.controller('EventController', function ($scope, $location, oauth2Pro
 		}
 	}
 
-
+	/* the container that holds all the rows of events that are loade rom the db */
 	$scope.container = jQuery(".events-container-wrapper");
 
+	/* 
+	 * I'm sure theres a better way of doing this but this way is definitely harder which makes me cooler lol jk. 
+	 * When loadin our events from the database, we have to essentially re-create datalist html if we want to present that way
+	 * so thats exactly what this function does. 
+	*/
 	$scope.createDataList = function(guests, index) {
 		if (guests.length > 0) {
 			var datalist_input = "<input list='%s'>".replace("%s", "guestlist"+index);
 			var datalist = "<datalist id='%s'>".replace("%s", "guestlist"+index )
+
 			guests.forEach(function(element, index) {
 				datalist += "<option value='%s'>".replace("%s", element);
 			});
 			datalist += "</datalist>";
+
 			return {input: datalist_input, datalist: datalist}
+
 		} else {
-			return {input: "no", datalist: "guests lol"}
+			return {input: "no", datalist: "guests"}
 		}
 	}
+
+	/* 
+	 * This method fetches all of the events from the db, organizes them intohtml and then displays them on the page
+     * Because I had so much troube with ng-repeat which would have made my life a whole lot easier in solving this problem,
+     * I had the pleaseure of learning a new templating system called muctache
+	*/
 	$scope.loadAllEventsOntoPage = function() {
 
+		// Declare our variables here so we dont have to keep re-creating objects which is more effient
 		var datalist, html, datalist_input, datalist_list, row1, row2, row3, row4, row5, row6, row7;
 		var request = gapi.client.user_endpoint.all_events();
-		request.execute(function(response){
-			response.events.forEach(function(element1, index) { 
-				datalist = $scope.createDataList(element1.event_guestlist, index);
-				element1.datalist_input = datalist.input;
-				element1.datalist_list = datalist.datalist;
-				element1.enableGuestList = function() {
 
-				}
-				log(element1)
+		// Start loading in events
+		request.execute(function(response){
+
+			// Iterate through the events
+			response.events.forEach(function(element1, index) { 
+
+				// For each event, re-create our datalist, since each datalist has two tags, an input and datalist
+				// this datalist will be an object containing two keys which correspond to the two tags, datalist.input, datalist.datalist
+				datalist = $scope.createDataList(element1.event_guestlist, index);
+
+				// now for each event key we create the html to display them using mustache to replace the values from out event objects
 				row1 = "<div class='row'><div class='col-md-3 text-left'><label for='event_name'>Name of Event: </label><span id='event_name'> {{event_name}}</span></div></div> ";
-				row2 = "<div class='row'><div class='col-md-3 text-center'><label for='event_type'>Type of Event: </label><span id='event_type'> {{event_type}}</span></div></div> "; 
-				row3 = "<div class='row'><div class='col-md-3 text-right'><label for='event_host'>Host of Event: </label><span id='event_host'> {{event_host}}</span></div></div> "; 
-				row4 = "<div class='row'><div class='col-md-4 text-right'><label for='event_start'>Event Start Date: </label><span id='event_start'> {{event_start}}</span></div></div> "; 
-				row5 = "<div class='row'><div class='col-md-5 text-right'><label for='event_end'>Event End Date: </label><span id='event_end'> {{event_end}}</span></div></div> "; 
-				row6 = "<div class='row'><div class='col-md-6 text-right'><label for='event_guestlist'>Event Guest list: </label><span id='event_guestlist' class='event_guestlist_unbelievable'>doit </span></div></div> ".replace("doit", datalist.input + datalist.datalist); 
-				row7 = "<div class='row'><div class='col-md-7 text-right'><label for='event_guestmessage'>Message for Guests: </label><span id='event_guestmessage'> {{event_guestmessage}}</span></div></div><hr> "; 
+				row2 = "<div class='row'><div class='col-md-3 text-left'><label for='event_type'>Type of Event: </label><span id='event_type'> {{event_type}}</span></div></div> "; 
+				row3 = "<div class='row'><div class='col-md-3 text-left'><label for='event_host'>Host of Event: </label><span id='event_host'> {{event_host}}</span></div></div> "; 
+				row4 = "<div class='row'><div class='col-md-3 text-left'><label for='event_start'>Event Start Date: </label><span id='event_start'> {{event_start}}</span></div></div> "; 
+				row5 = "<div class='row'><div class='col-md-3 text-left'><label for='event_end'>Event End Date: </label><span id='event_end'> {{event_end}}</span></div></div> "; 
+				row6 = "<div class='row'><div class='col-md-3 text-left'><label for='event_guestlist'>Event Guest list: </label><span id='event_guestlist' class='event_guestlist_unbelievable'>doit </span></div></div> ".replace("doit", datalist.input + datalist.datalist); 
+				row7 = "<div class='row'><div class='col-md-3 text-left'><label for='event_guestmessage'>Message for Guests: </label><span id='event_guestmessage'> {{event_guestmessage}}</span></div></div><hr> "; 
 				
+				// iterate through all the rows, use mustache to make the html and then add it onto the events container
 				[row1, row2, row3, row4, row5, row6, row7].forEach(function(element2, index){
 					html = Mustache.to_html(element2, element1);
 					$scope.container.append(html);
-					
 				});
 			});
 		});
